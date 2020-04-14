131e329191fc734ddfeda94cdd92a4b7827c6329
edkhachatryan <eduard.khachatrian@gmail.com>
2020-04-14 11:12:15 +0400

    object cleanup, chart config

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {DataService} from '../../services/data.service';
-import {interval, Subject, Subscription} from 'rxjs';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,37 +10,24 @@ import {interval, Subject, Subscription} from 'rxjs';
 export class HomeComponent implements OnInit, OnDestroy {
   public subscription: Subscription;
   public marketSummeries;
-  public filteredData; // : Subject<Array<any>>;
-  public marketArray = [ 'USD-ETH', 'USD-BSV', 'USD-USDT', 'USD-LINK'];
+  public filteredData;
+  public marketArray = [ 'USD-BTC', 'USD-ETH', 'USD-BSV', 'USD-USDT', 'USD-LINK', ];
 /*'USD-BTC',*/
 
-  // object contains the name,centures of the cricketers this is the data we will use to draw the chart
-  cricketersInfo;
-  // meta Info object holds lot of properties describes the title and color and othe meta info for chart
-  metaInfo;
+
+  public metaInfo;
 
   constructor(private dataService: DataService) {
     this.getMarketsSummery();
-
-    // adding data
-    this.cricketersInfo = [
-      {name: 'Sachin T', centuries: 49},
-      {name: 'Kohli  V', centuries: 43},
-      {name: 'Rohit  S', centuries: 28},
-      {name: ' Ganguly ', centuries: 22},
-      {name: 'Dhawan', centuries: 17},
-    ];
-
-    // Metadata for the chart like width and height of the chart, Title for the chart, Title color etc..
     this.metaInfo = {
       chartWidth: '800',
-      chartHeight: '800',
-      title: 'Indian cricketers with Most Centuries',
+      chartHeight: '500',
+      title: 'Bittrex current market data by value',
       titleColor: 'white',
       titleFont: '20px sans-serif',
       columnTitleColor: 'white',
       columnFont: '12px sans-serif',
-      footerTitle: 'Cricketer',
+      footerTitle: '',
       footerColor: '#c1d0cd',
       footerFont: '12px sans-serif',
       leftaxisColor: '#c1d0cd',
@@ -69,10 +56,8 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   filterData() {
-    // this.filteredData = new Subject<Array<any>>();
     if (this.marketSummeries) {
       this.filteredData = this.marketSummeries.filter( x => this.marketArray.map(y => y).includes(x.MarketName));
-      // this.filteredData.next(this.marketSummeries.filter( x => this.marketArray.map(y => y).includes(x.MarketName))) ;
       console.log('Filtered data', this.filteredData);
     }
   }
--- src/app/shared/chart/chart.component.ts
+++ src/app/shared/chart/chart.component.ts
@@ -8,75 +8,42 @@ import {Subject} from 'rxjs';
 })
 export class ChartComponent implements OnInit, OnChanges {
 
-  // getting the cricketers Information
   @Input() chartData;
-
-  // getting the chart Meta data
   @Input() chartMetaInfo;
 
-  changeLog: string[] = [];
-  context;
+
+  public context;
 
   constructor() {
   }
 
   ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
-    // setting up the inital Context, Get the canvas element
-    const canvas = document.getElementById('chart') as HTMLCanvasElement;
-    // getContext will return the rendering context using it we can call the different methods and properties for creating shapes
-    // we will be sending this context throught the methods to implement any drawing
-    this.context = canvas.getContext('2d');
-    // To set up the inital background color for the drawing area
-    this.context.fillStyle = '#262a33';
-    // fillRect will use the fillstyle color and starts from 0,0 position and draws a rectangle with the width and height provided in
-    // metadata , This will set up the context will #262a33 backgound , on top of this we will start drawing.
-    this.context.fillRect(0, 0, this.chartMetaInfo.chartWidth, this.chartMetaInfo.chartHeight);
-    // The below 4 functions , contain the logic of drawing the chart I will be explaining them in Steps sections
-    console.log('changes', changes.chartData.currentValue);
-    this.drawBarChart(this.context, changes.chartData.currentValue);
-    // this.drawBarChart(this.context, this.chartData);
-    this.addTitleToChart(this.context);
-    this.addFooterToChart(this.context);
-    this.addHorizontalLines(this.context);
-
-
-    // tslint:disable-next-line:forin
-/*    for (const propName in changes) {
-      const changedProp = changes[propName];
-      const to = JSON.stringify(changedProp.currentValue);
-      if (changedProp.isFirstChange()) {
-        log.push(`Initial value of ${propName} set to ${to}`);
-      } else {
-        const from = JSON.stringify(changedProp.previousValue);
-        log.push(`${propName} changed from ${from} to ${to}`);
-      }
-    }
-    this.changeLog.push(log.join(', '));*/
+    this.chartConfig(changes.chartData.currentValue);
   }
 
   ngOnInit() {
-    // setting up the inital Context, Get the canvas element
+    this.chartConfig(this.chartData);
+  }
+
+  chartConfig(chartData) {
     const canvas = document.getElementById('chart') as HTMLCanvasElement;
-    // getContext will return the rendering context using it we can call the different methods and properties for creating shapes
-    // we will be sending this context throught the methods to implement any drawing
+
     this.context = canvas.getContext('2d');
-    // To set up the inital background color for the drawing area
+
     this.context.fillStyle = '#262a33';
-    // fillRect will use the fillstyle color and starts from 0,0 position and draws a rectangle with the width and height provided in
-    // metadata , This will set up the context will #262a33 backgound , on top of this we will start drawing.
+
     this.context.fillRect(0, 0, this.chartMetaInfo.chartWidth, this.chartMetaInfo.chartHeight);
-    // The below 4 functions , contain the logic of drawing the chart I will be explaining them in Steps sections
-    this.drawBarChart(this.context, this.chartData);
+
+    this.drawBarChart(this.context, chartData);
     this.addTitleToChart(this.context);
     this.addFooterToChart(this.context);
     this.addHorizontalLines(this.context);
-
   }
 
   addTitleToChart(context) {
     context.font = this.chartMetaInfo.titleFont;
     context.fillStyle = this.chartMetaInfo.titleColor;
-    context.fillText(this.chartMetaInfo.title, 100, 30);
+    context.fillText(this.chartMetaInfo.title, 250, 30);
   }
 
   addFooterToChart(context) {
@@ -95,12 +62,12 @@ export class ChartComponent implements OnInit, OnChanges {
     context.font = this.chartMetaInfo.leftaxisFont;
     context.fillStyle = this.chartMetaInfo.leftaxisColor;
 
-    for (let i = 0; i < 11; i++) {
+    for (let i = 0; i < 21; i++) {
 
       context.lineWidth = 0.5;
       context.beginPath();
       context.moveTo(25, (20 * i) + 40);
-      context.lineTo(475, (20 * i) + 40);
+      context.lineTo(775, (20 * i) + 40);
       context.strokeStyle = this.chartMetaInfo.leftaxisColor;
       context.stroke();
     }
@@ -115,48 +82,17 @@ export class ChartComponent implements OnInit, OnChanges {
 
   drawBarChart(context, chartData) {
     console.log('chart data in drowing', chartData);
-    for (let cricketer = 0; cricketer < chartData.length; cricketer++) {
+    for (let market = 0; market < chartData.length; market++) {
       context.fillStyle = '#36b5d8';
-      const cricketerInfo = chartData[cricketer];
-      context.fillRect(25 + cricketer * 100,
-        this.chartMetaInfo.chartHeight - cricketerInfo.Bid * 2 - 60, 50,
-        cricketerInfo.Bid * 2);
-      this.addColumnName(context, cricketerInfo.MarketName, 25 + cricketer * 100, this.chartMetaInfo.chartHeight - 40);
-      this.addColumnHead(context, cricketerInfo.Bid, 45 + cricketer * 100,
-        this.chartMetaInfo.chartHeight - cricketerInfo.Bid * 2 - 65);
+      const marketInfo = chartData[market];
+      context.fillRect(25 + market * 100,
+        this.chartMetaInfo.chartHeight - marketInfo.Bid / 100 * 5 - 60, 50,
+        marketInfo.Bid / 100 * 5);
+      this.addColumnName(context, marketInfo.MarketName, 25 + market * 100, this.chartMetaInfo.chartHeight - 40);
+      this.addColumnHead(context, marketInfo.Bid, 28 + market * 100,
+        this.chartMetaInfo.chartHeight - marketInfo.Bid / 100 * 5 - 65);
     }
-
-
   }
-  /*
-  drawBarChart(context) {
-    console.log('chart data', this.chartData);
-    for (let cricketer = 0; cricketer < this.chartData.length; cricketer++) {
-      context.fillStyle = '#36b5d8';
-      const cricketerInfo = this.chartData[cricketer];
-      context.fillRect(25 + cricketer * 100,
-        this.chartMetaInfo.chartHeight - cricketerInfo.centuries * 2 - 60, 50,
-        cricketerInfo.centuries * 2);
-      this.addColumnName(context, cricketerInfo.MarketName, 25 + cricketer * 100, this.chartMetaInfo.chartHeight - 40);
-      this.addColumnHead(context, cricketerInfo.Bid, 45 + cricketer * 100,
-        this.chartMetaInfo.chartHeight - cricketerInfo.centuries * 2 - 65);
-    }
-
-
-  }*/
 
 }
 
-
-/*
- for (let cricketer = 0; cricketer < this.chartData.length; cricketer++) {
-      context.fillStyle = '#36b5d8';
-      const cricketerInfo = this.chartData[cricketer];
-      context.fillRect(25 + cricketer * 100,
-        this.chartMetaInfo.chartHeight - cricketerInfo.centuries * 2 - 60, 50,
-        cricketerInfo.centuries * 2);
-      this.addColumnName(context, cricketerInfo.MarketName, 25 + cricketer * 100, this.chartMetaInfo.chartHeight - 40);
-      this.addColumnHead(context, cricketerInfo.Bid, 45 + cricketer * 100,
-        this.chartMetaInfo.chartHeight - cricketerInfo.centuries * 2 - 65);
-    }
-*/
